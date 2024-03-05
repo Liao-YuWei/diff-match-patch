1d94b0bf52cd68bcaa56c1cef8619002b3c86753
unknown <awei6209@gmail.com>
2024-03-05 17:08:01 +0800

    draw arrows

--- javascript/line_diff.js
+++ javascript/line_diff.js
@@ -1,13 +1,7 @@
-// // 引入 diff-match-patch 库
-// const { diff_match_patch } = require('diff-match-patch');
-
-// const { render } = require('render');
-// import { render } from './render.js';
-
 // 定义函数来计算两个文本文件之间的差异
-function computeLineDiff(dmp, text1, text2) {
+function computeLineDiff(text1, text2) {
     // 创建 diff_match_patch 实例
-    // const dmp = new diff_match_patch();
+    const dmp = new diff_match_patch();
     // 使用 diff_linesToChars_ 函数来转换文本行
     const a = dmp.diff_linesToChars_(text1, text2);
     const lineText1 = a.chars1;
@@ -27,15 +21,12 @@ function computeLineDiff(dmp, text1, text2) {
     return diffHtml;
 }
 
-function readFile(inputId, outputId, text) {
+function readFile(inputId, text) {
     document.getElementById(inputId)
         .addEventListener('change', function () {
 
           let fr = new FileReader();
           fr.onload = function () {
-            // document.getElementById(outputId)
-            //   .textContent = fr.result;
-            // console.log(fr.result)
             text.str = fr.result;
           }
           try {
@@ -50,124 +41,57 @@ function readFile(inputId, outputId, text) {
 }
 
 function launchDiff() {
-    // console.log("-------in btn---------");
-    // console.log(`text1: ${text1.str}`);
-    // console.log(`text2: ${text2.str}`);
+    /**
+     * Compute diff
+     */
     var ms_start = (new Date()).getTime();
-    // Diff
-    const dmp = new diff_match_patch();
       
-    var diffs = computeLineDiff(dmp, text1.str, text2.str);
+    var diffs = computeLineDiff(text1.str, text2.str);
 
-    // Print diff in control panel
-    // console.log(diff);
-    // console.log("Differences:");
-    // diff.forEach(function(d) {
-    //   console.log(d[0], d[1]);
-    // });
-    // Show execution time
     var ms_end = (new Date()).getTime();
-    document.getElementById('diffSpeed').innerText = 'Time: ' + (ms_end - ms_start) / 1000 + 's';
+    document.getElementById('diffSpeed').innerText = 'Diff Time: ' + (ms_end - ms_start) / 1000 + 's';
 
-    // Render HTML on DOM
-    // document.getElementById("result").innerHTML = diffs;
-
-    // document.getElementById('originalFile').innerHTML = '';
-    // document.getElementById('newFile').innerHTML = '';
+    /** 
+     * Render graph on DOM
+     */
+    ms_start = (new Date()).getTime();
 
     const parser = new DOMParser();
     const doc = parser.parseFromString(diffs, 'text/html');
-    // console.log(doc);
     const hunks = doc.querySelectorAll('*');
-    // console.log(hunks);
     var diffArr = [];
-    // Pass the 3 root tags(<html>, <head>, <body>) and iterate through all tags
+    // Skip the 3 root tags(<html>, <head>, <body>) and iterate through all tags
     for (let i = 3; i < hunks.length; i++) {
         switch (hunks[i].tagName) {
             case 'SPAN':
-                // document.getElementById('originalFile').insertAdjacentHTML('beforeend', hunks[i].outerHTML);
-                // document.getElementById('newFile').insertAdjacentHTML('beforeend', hunks[i].outerHTML);
-                
-                var blocks = hunks[i].innerHTML.split("\n\u00B6<br>");
-                for (let j = 0; j < blocks.length; j++) {
-                    if (blocks[j] === '')
-                        continue;
-                    diffArr.push({
-                        block: `${blocks[j]}`,
-                        type: 'equal'
-                    });
-                }
-                // var content = document.createTextNode(hunks[i].innerText);
-                // content.innerHTML = hunks[i].innerHTML;
-                // oriItem.appendChild(content);
-
-                // var newItem = document.getElementById('newFile');
-                // console.log(hunks[i]);
+                diffArrPush(diffArr, hunks[i], 'equal');
                 break;
             case 'DEL':
-                // document.getElementById('originalFile').insertAdjacentHTML('beforeend', hunks[i].outerHTML);
-                // var brs = hunks[i].querySelectorAll('*');
-                // for (let j = 0; j < brs.length; j++)
-                //   document.getElementById('newFile').insertAdjacentHTML('beforeend', '<br>');
-
-                var blocks = hunks[i].innerHTML.split("\n\u00B6<br>");
-                for (let j = 0; j < blocks.length; j++) {
-                    if (blocks[j] === '')
-                        continue;
-                    diffArr.push({
-                        block: `${blocks[j]}`,
-                        type: 'delete'
-                    });
-                }
-                // console.log(hunks[i]);
+                diffArrPush(diffArr, hunks[i], 'delete');
                 break;
             case 'INS':
-                // var brs = hunks[i].querySelectorAll('*');
-                // for (let j = 0; j < brs.length; j++)
-                //     document.getElementById('originalFile').insertAdjacentHTML('beforeend', '<br>');
-                // document.getElementById('newFile').insertAdjacentHTML('beforeend', hunks[i].outerHTML);
-                
-                var blocks = hunks[i].innerHTML.split("\n\u00B6<br>");
-                for (let j = 0; j < blocks.length; j++) {
-                    if (blocks[j] === '')
-                        continue;
-                    diffArr.push({
-                        block: `${blocks[j]}`,
-                        type: 'insert'
-                    });
-                }
-                // console.log(hunks[i]);
+                diffArrPush(diffArr, hunks[i], 'insert');
                 break;
             default:
                 break;
-        }
-
-        // Pass <br> tag
-        // if (hunks[i].tagName !== "BR") {
-            
-        //     console.log(hunks[i]);
-        // }
-            
+        }           
     }
     render(diffArr);
-    // console.log(diffArr);
+
+    ms_end = (new Date()).getTime();
+    document.getElementById('renderSpeed').innerText = 'Render Time: ' + (ms_end - ms_start) / 1000 + 's';
+
     // console.log(doc.getElementsByTagName('*'));
 }
 
-// 示例用法
-// const text1 = `Hello
-// World
-// This is a test
-// `;
-// const text2 = `Hello
-// World
-// This is another test
-// `;
-
-// const differences = computeLineDiff(text1, text2);
-
-// // 输出差异
-// console.log("Differences:");
-// differences.forEach(([op, data]) => {
-//     console.log(op, data);
-// });
+function diffArrPush(diffArr, hunk, type) {
+    var blocks = hunk.innerHTML.split("\n\u00B6<br>");
+    for (let i = 0; i < blocks.length; i++) {
+        if (blocks[i] === '')
+            continue;
+        diffArr.push({
+            block: `${blocks[i]}`,
+            type: `${type}`
+        });
+    }
+}
--- javascript/render.js
+++ javascript/render.js
@@ -1,13 +1,13 @@
+const boxHeight = 50;
+const boxVerticalPadding = 25;
+
 function render(diffArr) {
     d3.select('#svgResult svg').remove(); // 刪除前一次建立的圖表
-
-    const boxWidth = 600, boxHeight = 50;
-    const boxVerticalPadding = 25;
 
     const rwdSvgWidth = parseInt(d3.select('#svgResult').style('width')),
         rwdSvgHeight = diffArr.length * (boxHeight + boxVerticalPadding);
     
-    const boxHorizontalPadding = rwdSvgWidth * 0.4;
+    const boxHorizontalPadding = rwdSvgWidth * 0.1;
     
     const svg = d3.select('#svgResult')
         .append('svg')
@@ -22,15 +22,37 @@ function render(diffArr) {
 
     // Define arrow head
     svg.append("svg:defs").append("svg:marker")
-        .attr("id", "arrow")
-        .attr("refX", 6)
-        .attr("refY", 6)
+        .attr("id", "black-arrow")
+        .attr("refX", 2)
+        .attr("refY", 2)
         .attr("markerWidth", 30)
         .attr("markerHeight", 30)
         .attr("orient", "auto")
         .append("path")
-        .attr("d", "M 0 0 12 6 0 12 3 6")
+        .attr("d", "M 0 0 4 2 0 4 1 2")
         .style("fill", "black");
+
+    svg.append("svg:defs").append("svg:marker")
+        .attr("id", "red-arrow")
+        .attr("refX", 2)
+        .attr("refY", 2)
+        .attr("markerWidth", 30)
+        .attr("markerHeight", 30)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M 0 0 4 2 0 4 1 2")
+        .style("fill", "red");
+
+    svg.append("svg:defs").append("svg:marker")
+        .attr("id", "green-arrow")
+        .attr("refX", 2)
+        .attr("refY", 2)
+        .attr("markerWidth", 30)
+        .attr("markerHeight", 30)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M 0 0 4 2 0 4 1 2")
+        .style("fill", "green");
 
     boxEnter
         .attr("transform", function(d, i) {
@@ -46,13 +68,7 @@ function render(diffArr) {
     
     var textWidth = [];
     boxEnter.append("text")
-        // .attr("transform",function(d,i) { 
-        //     return `translate(${this.getComputedTextLength() / 2}, 0)`
-        // })
         .text(d => d.block)
-        // .attr("x", function(d,i) { 
-        //     return `${-textWidth[i] / 2}` 
-        // })
         .attr("y", boxHeight / 2)
         .attr("text-anchor", "middle")
         .attr("dy", "0.35em")
@@ -61,8 +77,6 @@ function render(diffArr) {
             textWidth.push(this.getComputedTextLength() + 30);
         });
 
-    // console.log(textWidth);
-
     boxEnter.insert("rect", "text")
         .attr("width", (d, i) => `${textWidth[i]}`)
         .attr("height", boxHeight)
@@ -70,44 +84,74 @@ function render(diffArr) {
         .attr("transform",function(d,i) { 
                 return `translate(${-textWidth[i] / 2}, 0)`
         });
-
-    // const boxShift = 50;
-    // outerG.selectAll("g")
-    //     .filter(function(d) {
-    //         return d.type === "delete";
-    //     })
-    //     .attr("dx", boxShift);
     
-    var unfinishedSource = false, redChain = false, greenChain = false;
+    var redChain = false, greenChain = false;
     // var lastIsBlack = (diffArr[0].type === 'equal') ? true : false;
-    var lastRedId, lastGreenId;
-    var blocks = d3.selectAll("#svgResult svg g rect").nodes();
+    var lastBlackId = 0, lastRedId = 0, lastGreenId = 0;
+    if(diffArr[0].type === 'delete') {
+        redChain = true;
+        lastBlackId = -1;
+    } 
+    else if(diffArr[0].type === 'insert') {
+        greenChain = true;
+        lastBlackId = -1;
+    }
     const gHeight = boxHeight + boxVerticalPadding;
+    const blackX = 0, redX = -boxHorizontalPadding, greenX = boxHorizontalPadding;
+    const pathShift = 5;
     for (let i = 1; i < diffArr.length; i++) {
         switch(diffArr[i].type) {
             case 'equal':
                 if(redChain || greenChain) {
-                    if(redChain)
-                        drawArrow(outerG, -boxHorizontalPadding, gHeight * lastRedId + boxHeight, 0, gHeight * i);
-                    if(greenChain)
-                        drawArrow(outerG, boxHorizontalPadding, gHeight * lastGreenId + boxHeight, 0, gHeight * i);
+                    // From red to black
+                    if(redChain) {
+                        drawArrow(outerG, redX, gHeight * lastRedId + boxHeight, blackX - pathShift, gHeight * i, 'red', 1);
+                        redChain = false;
+                    }
+                    // From green to black
+                    if(greenChain) {
+                        drawArrow(outerG, greenX, gHeight * lastGreenId + boxHeight, blackX + pathShift, gHeight * i, 'green', 2);
+                        greenChain = false;
+                    }
                 }
+                // From black to black
                 else {
-                    drawArrow(outerG, 0, gHeight * i - boxVerticalPadding, 0, gHeight * i);
+                    drawArrow(outerG, blackX, gHeight * i - boxVerticalPadding, blackX, gHeight * i, 'black', 3);
                 }
-                lastIsBlack = true;
+                lastBlackId = i;
                 break;
 
             case 'delete':
-                
-                lastIsBlack = false;
+                // From red to red
+                if(redChain) {   
+                    drawArrow(outerG, redX, gHeight * (lastRedId + 1) - boxVerticalPadding, redX, gHeight * i, 'red', 4);
+                }
+                // From black to red
+                else {
+                    if(lastBlackId !== -1) {
+                        drawArrow(outerG, blackX - pathShift, gHeight * (lastBlackId + 1) - boxVerticalPadding, redX, gHeight * i, 'red', 5);
+                        lastBlackId = i - 1;    // Store last black id for the following green chain that may appear
+                    }
+                    redChain = true;
+                }
+                lastRedId = i;
                 break;
             
             case 'insert':
-
-                lastIsBlack = false;
+                // From green to green
+                if(greenChain) {
+                    drawArrow(outerG, greenX, gHeight * (lastGreenId + 1) - boxVerticalPadding, greenX, gHeight * i, 'green', 6);
+                }
+                // From black to green
+                else {
+                    if(lastBlackId !== -1) {
+                        drawArrow(outerG, blackX + pathShift, gHeight * (lastBlackId + 1) - boxVerticalPadding, greenX, gHeight * i, 'green', 7);
+                        lastBlackId = i - 1;    // Store last black id for the following green chain that may appear
+                    }   
+                    greenChain = true;                
+                }
+                lastGreenId = i;
                 break;
-
         }
     }
     
@@ -116,16 +160,7 @@ function render(diffArr) {
     // console.log(bounding_1.left, bounding_1.bottom, bounding_3.right, bounding_3.top)
 
     // drawArrow(outerG, bounding_1.left, bounding_1.bottom, bounding_3.right, bounding_3.top);
-    // drawArrow(outerG, 0, 50, -boxHorizontalPadding, 75);
     // console.log(d3.selectAll("#svgResult svg g rect").nodes()[1].getBoundingClientRect());
-    
-    // svg.append('line')
-    //     .attr('x1', 100)
-    //     .attr('y1', 100)
-    //     .attr('x2', 150)     
-    //     .attr('y2', 300)
-    //     .attr('stroke', 'blue')
-    //     .attr('marker-end', 'url(#arrow)');
 
     window.addEventListener("resize", handleResize);
 }
@@ -139,12 +174,44 @@ function handleResize() {
         .attr("transform", `translate(${rwdSvgWidth / 2}, 0)`);
 }
 
-function drawArrow(outerG, x1, y1, x2, y2) {
-    outerG.append('line')
-        .attr('x1', x1)
-        .attr('y1', y1)
-        .attr('x2', x2)     
-        .attr('y2', y2)
-        .attr('stroke', 'blue')
-        .attr('marker-end', 'url(#arrow)');
+function drawArrow(outerG, x1, y1, x2, y2, color, pathType) {
+    // outerG.append('line')
+    //     .attr('x1', x1)
+    //     .attr('y1', y1)
+    //     .attr('x2', x2)     
+    //     .attr('y2', y2)
+    //     .attr('stroke', color)
+    //     .attr("stroke-width", 3)
+    //     .attr('marker-end', `url(#${color}-arrow)`);
+    
+    switch(pathType) {
+        case 3: case 4: case 6: // Straight down arrow
+            outerG.append('path')
+                .attr('d', `M${x1} ${y1} L${x2} ${y2}`)
+                .attr('stroke', color)
+                .attr("stroke-width", 3)
+                .attr('marker-end', `url(#${color}-arrow)`);
+            break;
+
+        case 1: case 2: //Straight down then turn left or right arrow
+            outerG.append('path')
+                .attr('d', `M${x1} ${y1} L${x1} ${y2 - boxVerticalPadding / 2} L${x2} ${y2 - boxVerticalPadding / 2} L${x2} ${y2}`)
+                .attr('stroke', color)
+                .attr('stroke-width', 3)
+                .attr('fill', 'none')
+                .attr('marker-end', `url(#${color}-arrow)`);
+            break;
+
+        case 5: case 7: //Turn left or right then straight down and  arrow
+            outerG.append('path')
+                .attr('d', `M${x1} ${y1} L${x1} ${y1 + boxVerticalPadding / 2} L${x2} ${y1 + boxVerticalPadding / 2} L${x2} ${y2}`)
+                .attr('stroke', color)
+                .attr('stroke-width', 3)
+                .attr('fill', 'none')
+                .attr('marker-end', `url(#${color}-arrow)`);
+
+            break;
+    }
+
 }
+    
